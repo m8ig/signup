70960fb8ce19d05e6ea2ef74f69dd7ea43f4f6d3
m8ig <mike@sitio.ru>
2017-04-25 12:29:04 +0400

    The entry point

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
-// Components
-import Application from './assets/js/app.jsx';
+// Redux
+import { Provider } from "react-redux";
+import store from "./assets/js/store";
+import Application from "./assets/js/App";
+
+
+// import bootstrap-select
+require("bootstrap-sass/assets/javascripts/bootstrap/dropdown");
+require("bootstrap-select");
 
 
 // import styles
 import "./assets/sass/app.scss";
 
 
+// render
 const root = document.getElementById("root");
 
-ReactDOM.render(<Application />, root);
+ReactDOM.render(
+  <Provider store={store}>
+    <Application steps="3" />
+  </Provider>,
+  root
+);
